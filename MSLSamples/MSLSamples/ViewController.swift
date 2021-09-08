ee73a715ef742d86834188d2b0932ee08e10d95b
yyokii <yyokii.h@gmail.com>
2021-09-08 12:26:11 +0900

    ✨ initial settings

---
+++ MSLSamples/MSLSamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MSLSamples
+//
+//  Created by Higashihara Yoki on 2021/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
